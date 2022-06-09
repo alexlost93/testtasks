995ec023350353e100f847900c5a834fc1c8a2e9
Alex T <70171246+alexlost93@users.noreply.github.com>
2022-06-09 14:36:43 +0300

    123

--- numSearch.js
+++ numSearch.js
@@ -4,12 +4,11 @@ function oddTimesNumberSearch (arr) {
     let item;
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) { // for each arr[i] we check if arr[j] is equal and adding counter
-            if (arr[i] == arr[j]){
+            if (arr[i] == arr[j]) {
                 counter++;
             } 
 
-            if (entryCount < counter)
-            {
+            if (entryCount < counter) {
                 entryCount = counter;
                 item = arr[i];
             }
